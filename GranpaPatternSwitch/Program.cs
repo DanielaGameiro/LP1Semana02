0764e06d30f5272ae4eb03bcc208fcbbf77d4b56
Daniela Gameiro <danielagameiro@live.com.pt>
2023-03-08 16:30:37 +0000

    Add GranpaPatternSwitch project

---
+++ GranpaPatternSwitch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GranpaPatternSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variáveis auxiliares
+            string question;
+
+            // Colocar questão ao utilizador
+            Console.Write("Place your question? ");
+            question = Console.ReadLine();
+
+            // Switch por padrões
+            string response = question switch
+            {
+                "How are you?" => "I'm fine, thank you",
+                "What's your name?" => "Gran'pa",
+                "What's your mission?" => "Destroy mankind!",
+                _ => "You got me, I'm not THAT smart!",
+            };
+
+            // Responder adequadamente
+            Console.WriteLine(response);
+        }
+    }
+}
