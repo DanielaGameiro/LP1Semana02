1ad3accab60eeafd97e42112bbe4994b6bbe83aa
Daniela Gameiro <danielagameiro@live.com.pt>
2023-03-08 15:36:43 +0000

    Add max and min values from all types

---
+++ Specials/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Specials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"int max = {int.MaxValue}");
+            Console.WriteLine($"int min = {int.MinValue}");
+
+            Console.WriteLine($"long max = {long.MaxValue}");
+            Console.WriteLine($"long min = {long.MinValue}");
+
+            Console.WriteLine($"short max = {short.MaxValue}");
+            Console.WriteLine($"short min = {short.MinValue}");
+
+            Console.WriteLine($"sbyte max = {sbyte.MaxValue}");
+            Console.WriteLine($"sbyte min = {sbyte.MinValue}");
+
+            Console.WriteLine($"byte max = {byte.MaxValue}");
+            Console.WriteLine($"byte min = {byte.MinValue}");
+
+            Console.WriteLine($"ushort max = {ushort.MaxValue}");
+            Console.WriteLine($"ushort min = {ushort.MinValue}");
+
+            Console.WriteLine($"uint max = {uint.MaxValue}");
+            Console.WriteLine($"uint min = {uint.MinValue}");
+
+            Console.WriteLine($"ulong max = {ulong.MaxValue}");
+            Console.WriteLine($"ulong min = {ulong.MinValue}");
+
+            Console.WriteLine($"float max = {float.MaxValue}");
+            Console.WriteLine($"float min = {float.MinValue}");
+
+            Console.WriteLine($"double max = {double.MaxValue}");
+            Console.WriteLine($"double min = {double.MinValue}");
+
+            Console.WriteLine($"decimal max = {decimal.MaxValue}");
+            Console.WriteLine($"decimal min = {decimal.MinValue}");
+        }
+    }
+}
