77f5a5582d2621bb9c4e1ce0109b57c083794092
Daniela Gameiro <danielagameiro@live.com.pt>
2023-03-08 13:54:36 +0000

    Add a postfix increment operator to the exercise

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ManipulaVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(args[0]);
+
+            int a = 1, b;
+            b = a++ + x++; // a=2, x=input=2, b=2
+
+            Console.WriteLine($"O resultado da operação é {b}.");
+            Console.WriteLine($"O valor de A é {a}.");
+            Console.WriteLine($"O valor de X é {x}.");
+        }
+    }
+}
