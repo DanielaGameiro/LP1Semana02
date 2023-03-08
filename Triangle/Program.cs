d98d1bd6d2d722ed979149cb9cd3122021d2edec
Daniela Gameiro <danielagameiro@live.com.pt>
2023-03-08 17:05:15 +0000

    Add Triangle project

---
+++ Triangle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(args[0]);
+            char character = char.Parse(args[1]);
+
+            for (int row = 1; row <= numberOfLines; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write(character);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
+
