6bc92ac0182aff50314a44a52db04592f1952541
Daniela Gameiro <danielagameiro@live.com.pt>
2023-03-10 10:22:37 +0000

    Fix bug

--- ManipulaVar/Program.cs
+++ ManipulaVar/Program.cs
@@ -7,9 +7,10 @@ namespace ManipulaVar
         static void Main(string[] args)
         {
             int x = Convert.ToInt32(args[0]);
+            int y = x;
 
             int a = 1, b;
-            b = a++ + x++;
+            b = a++ + x++; 
 
             Console.WriteLine("");
             Console.WriteLine($"O resultado da operação é {b}.");
@@ -17,12 +18,12 @@ namespace ManipulaVar
             Console.WriteLine($"O valor de X é {x}.");
 
             int c = 1, d;
-            d = c++ + --x;
+            d = c++ + --y;
 
             Console.WriteLine("");
             Console.WriteLine($"O resultado da operação é {d}.");
             Console.WriteLine($"O valor de C é {c}.");
-            Console.WriteLine($"O valor de X é {x}.");
+            Console.WriteLine($"O valor de X é {y}.");
         }
     }
 }
