fade9736787ef6542a80899508a98f3f8c85cd10
Daniela Gameiro <danielagameiro@live.com.pt>
2023-03-10 12:09:35 +0000

    Add HardDiamond project

---
+++ HardDiamond/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HardDiamond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int item;
+
+            if (int.TryParse(args[0], out item))
+            {
+                if (item % 2 == 1)
+                {
+                    for (int i = 0; i < item; i++)
+                    {
+                        int middle = item / 2;
+                        int difference = Math.Abs(i - middle);
+
+                        for (int j = 0; j < item; j++)
+                        {
+                            if (difference + Math.Abs(j - middle) <= middle)
+                                Console.Write(".");
+                            
+                            else
+                                Console.Write(" ");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                else
+                    Console.WriteLine("I only want odd items!");
+            }
+        }
+    }
+}
